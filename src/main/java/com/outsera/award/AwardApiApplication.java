daea62f2fe59fc9cc7734c1d72f99233d06be964
João Pedro Gamin <joaopedrogamin@gmail.com>
2026-05-31 22:10:06 -0300

    chore: add Spring Boot application entry point

---
+++ src/main/java/com/outsera/award/AwardApiApplication.java
@@ -0,0 +1,11 @@
+package com.outsera.award;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwardApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AwardApiApplication.class, args);
+	}
+}
